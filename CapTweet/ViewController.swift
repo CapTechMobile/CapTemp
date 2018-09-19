ad6526ce198e25d30b04584354c6913813a67c2a
Alan Shih <ashih@captechconsulting.com>
2018-09-19 12:38:28 -0400

    Initial Commit

---
+++ CapTweet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapTweet
+//
+//  Created by Alan Shih on 9/19/18.
+//  Copyright © 2018 CapTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
